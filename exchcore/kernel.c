c732a87c3ecef4f930115c93bd9cd236f935b611
Terra1 <terrariola@gmail.com>
2019-11-15 10:49:02 +0100

    Massive update to kernel.c, first "real" code added.
    Note: Completely and utterly broken, needs to be fixed to run.

--- exchcore/kernel.c
+++ exchcore/kernel.c
@@ -1,8 +1,24 @@
+/* FVA (Function/Variable/Array) Name List:
+wtvm: Write to Video Memory
+wltx: Welcome Text
+c1: Counter 1
+*/
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-int started
-char welcome
-int main() {
-	(char*) 0xB0000
-	
+int c1;
+char wltx[7] = { "W", "e", "l", "c", "o", "m" "e" }; //Array containing the word "welcome" in individual characters.
+void wtvm() //Creates the "Write to Video Memory" function.
+{
+	char *wtvm = (char*)0xB8000; //Creates a pointer to the start of VGA video memory named "wtvm".
+	for (c1 != 7; c1 < 8; c1++) //For loop, executes 7 times.
+	{
+		*wtvm = wltx[c1]; //Writes "Welcome" to video memory.
+		wtvm++; //Increments "wtvm" by 1.
+	}
+}
+
+kernel_main()
+{
+	wtvm(); //Executes the function "wtvm" (located at line 11).
+}
