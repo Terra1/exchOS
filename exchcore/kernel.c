46adc720217e8d535c3e3c26f0c836c588a5f5d7
Terra1 <terrariola@gmail.com>
2019-11-17 19:32:28 +0100

    Thanks, Teln0
    Note: Tested, broken.

--- exchcore/kernel.c
+++ exchcore/kernel.c
@@ -1,22 +1,21 @@
 /* FVA (Function/Variable/Array) Name List:
 wtvm: Write to Video Memory
 wltx: Welcome Text
-c1: Counter 1
+pos: Counter
 */
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-int c1;
-char wltx[7] = { "W", "e", "l", "c", "o", "m" "e" }; //Array containing the word "welcome" in individual characters.
+char wltx[8] = { 'W', 'e', 'l', 'c', 'o', 'm', 'e', '\0' }; //Array containing the word "welcome" in individual characters, terminated by null.
 void wtvm() //Creates the "Write to Video Memory" function.
 {
 	char *wtvm = (char*)0xB8000; //Creates a pointer to the start of VGA video memory named "wtvm".
-	for (c1 != 7; c1 < 8; c1++) //For loop, executes 7 times.
+	int pos = 0;
+	while ( wltx[pos] != '\0' )
 	{
-		*wtvm = wltx[c1]; //Writes "Welcome" to video memory.
-		wtvm++; //Increments
-		wtvm++; //wtvm
-		wtvm++; //by three.
+		*wtvm = wltx[pos]; //Writes "Welcome" to video memory.
+		wtvm += 3;
+		pos++;
 	}
 }
 
