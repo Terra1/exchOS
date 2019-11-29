a3bc8813006f1cbfe9b9125c7c45e59e559a88d1
Terra1 <terrariola@gmail.com>
2019-11-29 09:59:50 +0100

    You're an idiot. -Everybody at #osdev.

--- exchcore/earlyboot.c
+++ exchcore/earlyboot.c
@@ -1,31 +1,34 @@
-/* FVA (Function/Variable/Array) Name List:
-wtvm: Write to Video Memory
-wltx: Welcome Text
-pos: Counter
+/*
+ FVA (Function/Variable/Array) Name List:
+ write_vidmem: Write to Video Memory
+ wel_text: Welcome Text
+ pos: Counter
 */
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
-char wltx[8] = { 'W', 'e', 'l', 'c', 'o', 'm', 'e', '\0' }; //Array containing the word "welcome" in individual characters, terminated by null.
-void wtvm() //Creates the "Write to Video Memory" function.
+
+char welc_text[8] = { 'W', 'e', 'l', 'c', 'o', 'm', 'e', '\0' }; // Array containing the word "welcome" in individual characters, terminated by null.
+static void write_vidmem() // Creates the "Write to Video Memory" function.
 {
-	volatile char *wtvm = (volatile char*)0xB8000; //Creates a pointer to the start of VGA video memory named "wtvm".
+	volatile char *vidmem = (volatile char*)0xB8000; // Creates a pointer to the start of VGA video memory named "vidmem".
 	int pos = 0;
 	while ( wltx[pos] != '\0' )
 	{
-		*wtvm = wltx[pos]; //Writes "Welcome" to video memory.
-		wtvm += 4;
+		*vidmem = welc_text[pos]; // Writes "Welcome" to video memory.
+		vidmem += 4;
 		pos++;
 	}
 }
 
-void gdt() //Creates the function used for the Global Descriptor Table
-}
-	asm()
+static void gdt() // Creates the function used for the Global Descriptor Table
 {
+	asm()
+}
 
 int start_main()
 {
-	wtvm(); //Executes the function "wtvm" (located at line 10).
-	gdt(); //Executes the function "gdt" (located at line 22).
+	write_vidmem(); // Executes the function "write_vidmem" (located at line 13).
+	gdt(); // Executes the function "gdt" (located at line 25).
 }
